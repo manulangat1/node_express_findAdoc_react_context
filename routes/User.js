8f4c1fd6bf6ff097e4dcac02f51b56d43580247d
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-12 00:15:02 +0300

    ft:adding_is_Admin_middleware:backend

--- routes/User.js
+++ routes/User.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { registerUser,loginUser , loadUser} = require('../controllers/User')
 const auth = require('../middlewares/isAuth')
+const isAdmin = require('../middlewares/isAdmin')
 
 const router = express.Router()
 
 router.route('/').post(registerUser)
 router.route('/login/').post(loginUser)
-router.route('/user/').get(auth,loadUser)
+router.route('/user/').get(auth,isAdmin,loadUser)
 module.exports = router
