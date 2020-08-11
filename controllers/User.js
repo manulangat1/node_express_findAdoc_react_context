0b18a135e065496ab21c07c5469bc8bfa67540d2
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-11 21:40:34 +0300

    ft:adding_register_login_controller:backend

---
+++ controllers/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const User = require('../models/User')
+
+
+exports.registerUser = async(req,res,next) => {
+    try{
+        const { email,username,password,isAdmin } = req.body 
+        const user = await User.create(req.body)
+        const token = await user.generateAuthToken()
+        res.status(201).json({
+            success:true,
+            data:user,
+            token:token
+        })
+    } catch  (err){
+        console.log(`err:${err.message}`)
+        res.status(500).json({
+            success:false,
+            message:'Internal Server Error'
+        })
+    }
+}
+
+
+exports.loginUser = async(req,res,next) => {
+    try{
+        const {  email,password } = req.body 
+        const user = await User.findByCredentials(email,password)
+        const token = await user.generateAuthToken()
+        res.status(200).json({
+            success:true,
+            data:user,
+            token:token
+        })
+    } catch (err){
+        console.log(`err:${err.message}`)
+        res.status(500).json({
+            success:false,
+            message:'Internal Server Error'
+        })
+    }
+}
